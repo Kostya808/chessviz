fc5b71ea37f453b4c223c1d23cd3a45512ce4f53
ljkdm <ljkd@example.com>
2018-03-20 19:41:41 +0700

    Правильное создание и вывод доски

---
+++ board.h
@@ -0,0 +1,11 @@
+typedef struct board {
+    int str; //На какой строке находится пешка
+    char stl; //На каком столбце находится пешка
+    int color; //Цвет фигуры
+    char tip;
+}board;
+
+void output();
+
+
+#include <stdio.h>
